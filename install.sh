04f646cb53405a0d3f676027bfb77974560176ac
wegare123 <56117745+wegare123@users.noreply.github.com>
2021-01-24 07:52:36 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#tjt (Wegare)
+printf 'ctrl+c' | crontab -e > /dev/null
+opkg update && opkg install unzip
+cek=$(cat /etc/openwrt_r*)
+if [[ $cek == *"LEDE"* ]] && [[ $cek == *"ar71xx"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/lede/ar71xx.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ar71xx/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ar71xx
+elif [[ $cek == *"LEDE"* ]] && [[ $cek == *"brcm63xx"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/lede/brcm63xx.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/brcm63xx/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/brcm63xx
+elif [[ $cek == *"LEDE"* ]] && [[ $cek == *"ramips"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/lede/ramips.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ramips/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ramips
+elif [[ $cek == *"LEDE"* ]] && [[ $cek == *"sunxi"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/lede/sunxi.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/sunxi/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/sunxi
+elif [[ $cek == *"Chaos Calmer"* ]] && [[ $cek == *"ar71xx"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/cc/ar71xx.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ar71xx/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ar71xx
+elif [[ $cek == *"Chaos Calmer"* ]] && [[ $cek == *"brcm63xx"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/cc/brcm63xx.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/brcm63xx/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/brcm63xx
+elif [[ $cek == *"Chaos Calmer"* ]] && [[ $cek == *"ramips"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/cc/ramips.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ramips/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ramips
+elif [[ $cek == *"Chaos Calmer"* ]] && [[ $cek == *"sunxi"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/cc/sunxi.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/sunxi/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/sunxi
+elif [[ $cek == *"OpenWrt"* ]] && [[ $cek == *"aarch64_cortex-a53"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/openwrt/sunxi.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/sunxi/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/sunxi
+elif [[ $cek == *"OpenWrt"* ]] && [[ $cek == *"mips_24kc"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/openwrt/ar71xx.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ar71xx/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ar71xx
+elif [[ $cek == *"OpenWrt"* ]] && [[ $cek == *"mipsel_24kc"* ]]; then
+wget --no-check-certificate "https://github.com/wegare123/backup/blob/main/openwrt/ramips.zip?raw=true" -O ~/ekstrak.zip && unzip ~/ekstrak.zip && cp ~/ramips/*.ipk ~/ && rm -rf ~/ekstrak.zip && rm -rf ~/ramips
+else
+echo -e "version anda tidak terdeteksi!"
+exit
+fi
+wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/tjt/main/tjt.sh" -O /usr/bin/tjt
+wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/tjt/main/trojan" -O /usr/bin/trojan
+wget --no-check-certificate "https://raw.githubusercontent.com/wegare123/tjt/main/autorekonek-tjt.sh" -O /usr/bin/autorekonek-tjt
+opkg install ip-full ipset lsof fping && opkg install *.ipk
+chmod +x /usr/bin/tjt
+chmod +x /usr/bin/autorekonek-tjt
+chmod +x /usr/bin/trojan
+rm -r ~/*.ipk
+rm -r ~/install.sh
+mkdir -p ~/akun/
+touch ~/akun/tjt.txt
+sleep 2
+echo "install selesai"
+echo "untuk memulai tools silahkan jalankan perintah 'tjt'"
+#echo "silahkan reboot terlebih dahulu"
+
+
+				
---
+++ tjt.sh
@@ -0,0 +1,170 @@
+#!/bin/bash
+#tjt (Wegare)
+clear
+udp2="$(cat /root/akun/tjt.txt | grep -i udp | cut -d= -f2)" 
+host2="$(cat /root/akun/tjt.txt | grep -i host | cut -d= -f2 | head -n1)" 
+port2="$(cat /root/akun/tjt.txt | grep -i port | cut -d= -f2)" 
+bug2="$(cat /root/akun/tjt.txt | grep -i bug | cut -d= -f2)" 
+pass2="$(cat /root/akun/tjt.txt | grep -i pass | cut -d= -f2)" 
+echo "Inject trojan by wegare"
+echo "1. Sett Profile"
+echo "2. Start Inject"
+echo "3. Stop Inject"
+echo "4. Enable auto booting & auto rekonek"
+echo "5. Disable auto booting & auto rekonek"
+echo "e. exit"
+read -p "(default tools: 2) : " tools
+[ -z "${tools}" ] && tools="2"
+if [ "$tools" = "1" ]; then
+
+echo "Masukkan host/ip" 
+read -p "default host/ip: $host2 : " host
+[ -z "${host}" ] && host="$host2"
+
+echo "Masukkan port" 
+read -p "default port: $port2 : " port
+[ -z "${port}" ] && port="$port2"
+
+echo "Masukkan password/key" 
+read -p "default password/key: $pass2 : " pass
+[ -z "${pass}" ] && pass="$pass2"
+
+echo "Masukkan bug" 
+read -p "default bug: $bug2 : " bug
+[ -z "${bug}" ] && bug="$bug2"
+
+read -p "ingin menggunakan port udpgw y/n " pilih
+if [ "$pilih" = "y" ]; then
+echo "Masukkan port udpgw" 
+read -p "default udpgw: $udp2 : " udp
+[ -z "${udp}" ] && udp="$udp2"
+badvpn="--socks-server-addr 127.0.0.1:1080 --udpgw-remote-server-addr 127.0.0.1:$udp"
+elif [ "$pilih" = "Y" ]; then
+echo "Masukkan port udpgw" 
+read -p "default udpgw: $udp2 : " udp
+[ -z "${udp}" ] && udp="$udp2"
+badvpn="--socks-server-addr 127.0.0.1:1080 --udpgw-remote-server-addr 127.0.0.1:$udp"
+else
+badvpn="--socks-server-addr 127.0.0.1:1080"
+fi
+
+echo "host=$host
+port=$port
+pass=$pass
+bug=$bug
+udp=$udp" > /root/akun/tjt.txt
+cat <<EOF> /root/akun/tjt.json
+{
+    "run_type": "client",
+    "local_addr": "127.0.0.1",
+    "local_port": 1080,
+    "remote_addr": "$host",
+    "remote_port": $port,
+    "password": [
+        "$pass"
+    ],
+    "log_level": 1,
+    "ssl": {
+        "verify": false,
+        "verify_hostname": true,
+        "cert": "",
+        "cipher": "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES128-SHA:ECDHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA:AES128-SHA:AES256-SHA:DES-CBC3-SHA",
+        "cipher_tls13": "TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384",
+        "sni": "$bug",
+        "alpn": [
+            "h2",
+            "http/1.1"
+        ],
+        "reuse_session": true,
+        "session_ticket": false,
+        "curves": ""
+    },
+    "tcp": {
+        "no_delay": true,
+        "keep_alive": true,
+        "reuse_port": false,
+        "fast_open": false,
+        "fast_open_qlen": 20
+    }
+}
+
+EOF
+cat <<EOF> /usr/bin/gproxy-tjt
+badvpn-tun2socks --tundev tun1 --netif-ipaddr 10.0.0.2 --netif-netmask 255.255.255.0 $badvpn --udpgw-connection-buffer-size 65535 --udpgw-transparent-dns &
+EOF
+chmod +x /usr/bin/gproxy-tjt
+echo "Sett Profile Sukses"
+sleep 2
+clear
+/usr/bin/tjt
+elif [ "${tools}" = "2" ]; then
+ipmodem="$(route -n | grep -i 0.0.0.0 | head -n1 | awk '{print $2}')" 
+echo "ipmodem=$ipmodem" > /root/akun/ipmodem.txt
+udp="$(cat /root/akun/tjt.txt | grep -i udp | cut -d= -f2)" 
+host="$(cat /root/akun/tjt.txt | grep -i host | cut -d= -f2 | head -n1)" 
+route="$(cat /root/akun/ipmodem.txt | grep -i ipmodem | cut -d= -f2 | tail -n1)"
+
+trojan -c /root/akun/tjt.json &
+sleep 3
+ip tuntap add dev tun1 mode tun
+ifconfig tun1 10.0.0.1 netmask 255.255.255.0
+/usr/bin/gproxy-tjt
+route add 8.8.8.8 gw $route metric 0
+route add 8.8.4.4 gw $route metric 0
+route add $host gw $route metric 0
+route add default gw 10.0.0.2 metric 0
+cat <<EOF> /usr/bin/ping-tjt
+#!/bin/bash
+#tjt (Wegare)
+while :
+do
+fping -c1 10.0.0.2
+sleep 1
+done
+EOF
+chmod +x /usr/bin/ping-tjt
+/usr/bin/ping-tjt > /dev/null 2>&1 &
+sleep 5
+elif [ "${tools}" = "3" ]; then
+host="$(cat /root/akun/tjt.txt | grep -i host | cut -d= -f2 | head -n1)" 
+route="$(cat /root/akun/ipmodem.txt | grep -i ipmodem | cut -d= -f2 | tail -n1)" 
+#killall screen
+killall -q badvpn-tun2socks trojan ping-tjt
+route del 8.8.8.8 gw "$route" metric 0 2>/dev/null
+route del 8.8.4.4 gw "$route" metric 0 2>/dev/null
+route del "$host" gw "$route" metric 0 2>/dev/null
+ip link delete tun1 2>/dev/null
+killall dnsmasq 
+/etc/init.d/dnsmasq start > /dev/null
+sleep 2
+echo "Stop Suksess"
+sleep 2
+clear
+/usr/bin/tjt
+elif [ "${tools}" = "4" ]; then
+cat <<EOF>> /etc/crontabs/root
+
+# BEGIN AUTOREKONEKTJT
+*/1 * * * *  autorekonek-tjt
+# END AUTOREKONEKTJT
+EOF
+sed -i 's/exit 0/ /g' /etc/rc.local
+/etc/init.d/cron restart
+echo "Enable Suksess"
+sleep 2
+clear
+/usr/bin/tjt
+elif [ "${tools}" = "5" ]; then
+sed -i "/^# BEGIN AUTOREKONEKTJT/,/^# END AUTOREKONEKTJT/d" /etc/crontabs/root > /dev/null
+/etc/init.d/cron restart
+echo "Disable Suksess"
+sleep 2
+clear
+/usr/bin/tjt
+elif [ "${tools}" = "e" ]; then
+clear
+exit
+else 
+echo -e "$tools: invalid selection."
+exit
+fi
