ca26667e40e03235a58db449cb825b71c56d3e44
wegare123 <56117745+wegare123@users.noreply.github.com>
2021-02-14 04:13:42 +0700

    Add files via upload

--- tjt.sh
+++ tjt.sh
@@ -125,7 +125,7 @@ elif [ "${tools}" = "3" ]; then
 host="$(cat /root/akun/tjt.txt | grep -i host | cut -d= -f2 | head -n1)" 
 route="$(cat /root/akun/ipmodem.txt | grep -i ipmodem | cut -d= -f2 | tail -n1)" 
 #killall screen
-killall -q badvpn-tun2socks trojan ping-tjt
+killall -q badvpn-tun2socks trojan ping-tjt fping
 route del 8.8.8.8 gw "$route" metric 0 2>/dev/null
 route del 8.8.4.4 gw "$route" metric 0 2>/dev/null
 route del "$host" gw "$route" metric 0 2>/dev/null
